4e318a32826e365d22f02ad289a956a9fec2547b
huangyl_oup <huangyl_oup@si-tech.com.cn>
2023-02-21 21:09:38 +0800

    add by huangyl

--- cloudnote-admin/src/main/java/com/hyl/cloudnote/config/FileHandleConfig.java
+++ cloudnote-admin/src/main/java/com/hyl/cloudnote/config/FileHandleConfig.java
@@ -5,9 +5,24 @@ import org.springframework.util.ResourceUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+
+/**
+ * @description : 拦截器配置
+ * @param : 
+ * @return : 
+ * @author : huangyl
+ * @time : 2023/2/18
+ */
 @Configuration
 public class FileHandleConfig  extends WebMvcConfigurationSupport {
 
+	/**
+	 * @description : 访问静态文件
+	 * @param : [registry]
+	 * @return : void
+	 * @author : huangyl
+	 * @time : 2023/2/18
+	 */
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
